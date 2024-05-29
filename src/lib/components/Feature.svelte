470c080818cb4ec652f1ab41c4856b78e614dad4
Senna46 <29295263+Senna46@users.noreply.github.com>
2024-05-29 15:15:18 +0900

    feat: features order

--- src/lib/components/Feature.svelte
+++ src/lib/components/Feature.svelte
@@ -16,6 +16,24 @@
 	// Define your carousel items
 	const items = [
 		{
+			id: 4,
+			image: images.feature4,
+			label: 'sunrise-features.feature-4.label',
+			description: 'sunrise-features.feature-4.description'
+		},
+		{
+			id: 5,
+			image: images.feature5,
+			label: 'sunrise-features.feature-5.label',
+			description: 'sunrise-features.feature-5.description'
+		},
+		{
+			id: 3,
+			image: images.feature3,
+			label: 'sunrise-features.feature-3.label',
+			description: 'sunrise-features.feature-3.description'
+		},
+		{
 			id: 1,
 			image: images.feature1,
 			label: 'sunrise-features.feature-1.label',
@@ -26,25 +44,7 @@
 			image: images.feature2,
 			label: 'sunrise-features.feature-2.label',
 			description: 'sunrise-features.feature-2.description'
-		},
-		{
-			id: 3,
-			image: images.feature3,
-			label: 'sunrise-features.feature-3.label',
-			description: 'sunrise-features.feature-3.description'
-		},
-		{
-			id: 5,
-			image: images.feature5,
-			label: 'sunrise-features.feature-5.label',
-			description: 'sunrise-features.feature-5.description'
-		},
-		{
-			id: 4,
-			image: images.feature4,
-			label: 'sunrise-features.feature-4.label',
-			description: 'sunrise-features.feature-4.description'
-		},
+		}
 	];
 
 	let currentIndex = writable(0);
@@ -55,11 +55,7 @@
 		<div
 			class="md:min-w-[400px] min-w-[310px] gap-3 py-4 px-3 text-white rounded-[20px] border-2 border-solid bg-white"
 		>
-			<img
-				class="md:w-full w-[60%] max-w-60 mx-auto max-h-60"
-				src={item.image}
-				alt="item"
-			/>
+			<img class="md:w-full w-[60%] max-w-60 mx-auto max-h-60" src={item.image} alt="item" />
 			<div class="sm:px-3 mt-4">
 				<Label
 					className={'px-2 font-orbitron 2xl:text-3xl md:text-2xl text-xl font-extrabold tracking-wider text-black'}
