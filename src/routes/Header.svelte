9227b6092e094430e8b8573d99ce8a61b71b61fd
Senna46 <29295263+Senna46@users.noreply.github.com>
2024-06-18 04:01:04 +0900

    feat: new header & footer

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,127 +1,94 @@
 <script>
 	import { page } from '$app/stores';
-	import color from '$lib/images/color.svg';
-
-	import { appTitle } from '../consts';
-
+	import * as images from '../consts/images';
 	let isEnglish = true;
+	import { newLocale } from '../lib/stores/i18nStore';
+	import { locale } from 'svelte-i18n';
 
 	function toggleLanguage() {
 		isEnglish = !isEnglish;
+		locale.set(isEnglish ? 'en' : 'jp');
+		newLocale.set(isEnglish ? 'en' : 'jp');
 	}
 </script>
 
 <header>
-	<div class="corner flex gap-2 items-center text-2xl">
-		<img src={color} alt="SvelteKit" />
-		<span>{appTitle}</span>
+	<div class="flex flex-row px-4 md:px-20 py-2">
+		<details class="dropdown sm:hidden">
+			<summary class="btn btn-square btn-ghost">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					class="inline-block w-5 h-5 stroke-current"
+					style="filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(95deg) brightness(100%) contrast(100%);"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h16M4 18h16"
+					></path>
+				</svg>
+			</summary>
+			<ul class="p-2 shadow menu dropdown-content z-50 bg-base-100 rounded-box w-52">
+				<li><a href="https://docs.sunriselayer.io" target="_blank">Learn</a></li>
+				<li>
+					<a href="https://docs.sunriselayer.io/run-a-sunrise-node/networks" target="_blank"
+						>Build</a
+					>
+				</li>
+				<li>
+					<button on:click={toggleLanguage} class="items-center">
+						{isEnglish ? 'EN' : 'JP'}
+					</button>
+				</li>
+			</ul>
+		</details>
+		<div class="flex gap-4 items-center text-2xl">
+			<img
+				class="fill-current text-white"
+				style="filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(95deg) brightness(100%) contrast(100%);"
+				src={images.logo}
+				alt="logo"
+			/>
+			<img
+				style="filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(95deg) brightness(100%) contrast(100%);"
+				src={images.sunrise}
+				alt="sunrise"
+			/>
+		</div>
+		<span class="flex-auto"></span>
+
+		<div class="hidden sm:flex items-center">
+			<nav>
+				<ul class="flex gap-8 text-2xl text-white">
+					<li
+						class="text-white"
+						aria-current={$page.url.pathname === '/learn' ? 'page' : undefined}
+					>
+						<a href="https://docs.sunriselayer.io" target="_blank">Learn</a>
+					</li>
+					<li
+						class="text-white"
+						aria-current={$page.url.pathname === '/build' ? 'page' : undefined}
+					>
+						<a href="https://docs.sunriselayer.io/run-a-sunrise-node/networks" target="_blank"
+							>Build</a
+						>
+					</li>
+					<!-- <li
+								class="text-white"
+								aria-current={$page.url.pathname === '/run-node' ? 'page' : undefined}
+							>
+								<a href="/">Run a Node</a>
+							</li> -->
+					<li class="text-white" aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+						<button on:click={toggleLanguage} class="items-center">
+							{isEnglish ? 'EN' : 'JP'}
+						</button>
+					</li>
+				</ul>
+			</nav>
+		</div>
 	</div>
-
-	<nav class="items-center">
-		<ul>
-			<li class="flex items-center" aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<button on:click={toggleLanguage} class="items-center">
-					{isEnglish ? 'English' : 'Japanese'}
-				</button>
-			</li>
-			<li aria-current={$page.url.pathname === '/learn' ? 'page' : undefined}>
-				<a href="https://docs.sunriselayer.io" target="_blank">Learn</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/build' ? 'page' : undefined}>
-				<a href="https://docs.sunriselayer.io/run-a-sunrise-node/networks" target="_blank">Build</a>
-			</li>
-			<!-- <li aria-current={$page.url.pathname === '/run-node' ? 'page' : undefined}>
-				<a href="/run-node">Run a Node</a>
-			</li> -->
-		</ul>
-	</nav>
 </header>
-
-<style>
-	header {
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
-	nav {
-		display: flex;
-		justify-content: center;
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 0.5rem;
-		color: var(--color-text);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--color-theme-1);
-	}
-</style>
