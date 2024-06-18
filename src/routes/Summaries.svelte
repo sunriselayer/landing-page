fe14cf45bd6c12b5b9f385ca9424cdecd54d9e50
Senna46 <29295263+Senna46@users.noreply.github.com>
2024-06-18 17:34:30 +0900

    feat: separate summary

--- src/routes/Summaries.svelte
+++ src/routes/Summaries.svelte
@@ -1,61 +1,17 @@
 <script lang="ts">
 	import { _ } from 'svelte-i18n';
-	import Label from '$lib/components/Label.svelte';
-	import Feature from '$lib/components/Feature.svelte';
-	import * as images from '../consts/images';
+	import Summary from '$lib/components/Summary.svelte';
 </script>
 
 <main>
 	<section class="gap-8 my-8">
-		<h1 class="text-center font-orbitron text-4xl font-extrabold md:text-5xl xl:text-6xl">
+		<h1 class="text-center font-orbitron font-extrabold text-4xl md:text-5xl xl:text-6xl">
 			{@html $_('sunrise-products.label')}
 		</h1>
-		<h2 class="md:text:xl text-center font-overpass sm:text-lg xl:text-2xl">
+		<h2 class="md:text:xl text-center font-overpass text-sm sm:text-lg xl:text-2xl">
 			{@html $_('sunrise-products.description')}
 		</h2>
-
-		<div
-			class="card md:flex-row max-w-5xl"
-			style="background: linear-gradient(180deg, #182949 0%, #213457 100%)"
-		>
-			<img class="max-w-60 mx-auto max-h-60 p-4" src={images.validiums} alt="validiums" />
-			<div class="card-body">
-				<h2 class="card-title text-2xl font-sans text-white">
-					{$_('sunrise-products.product-validiums.label')}
-				</h2>
-				<p class="text-white font-sans">
-					{@html $_('sunrise-products.product-validiums.description')}
-				</p>
-			</div>
-		</div>
-		<div
-			class="card md:flex-row max-w-5xl"
-			style="background: linear-gradient(180deg, #182949 0%, #213457 100%)"
-		>
-			<img class="max-w-60 mx-auto max-h-60 p-4" src={images.rollups} alt="rollups" />
-			<div class="card-body">
-				<h2 class="card-title text-2xl font-sans text-white">
-					{$_('sunrise-products.product-rollups.label')}
-				</h2>
-				<p class="text-white font-sans">
-					{@html $_('sunrise-products.product-rollups.description')}
-				</p>
-			</div>
-		</div>
-		<div
-			class="card md:flex-row max-w-5xl"
-			style="background: linear-gradient(180deg, #182949 0%, #213457 100%)"
-		>
-			<img class="max-w-60 mx-auto max-h-60 p-4" src={images.pol} alt="pol" />
-			<div class="card-body">
-				<h2 class="card-title text-2xl font-sans text-white">
-					{$_('sunrise-products.product-pol.label')}
-				</h2>
-				<p class="text-white font-sans">
-					{@html $_('sunrise-products.product-pol.description')}
-				</p>
-			</div>
-		</div>
+		<Summary />
 	</section>
 </main>
 
