04d60f364c53c2b55caf8eb6477e25abcc5d93a6
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2025-05-12 23:54:10 +0800

    link

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -4,4 +4,4 @@ export const testNetLink = 'https://testnet.app.sunriselayer.io/';
 export const buildLink = '/build';
 export const deployLink = 'https://docs.sunriselayer.io/build/quick-start';
 export const learnLink = 'https://docs.sunriselayer.io/learn/sunrise';
-export const docsLink = '/#';
+export const docsLink = 'https://docs.sunriselayer.io/learn/sunrise';
